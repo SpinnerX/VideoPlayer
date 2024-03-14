30533ce5050bbddd2b86914bab2eb0feb11e0e0a
type <type>
2024-03-14 04:34:04 -0700

    FFMPEG Stuff

---
+++ build/CMakeFiles/VideoPlayer.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/aaronher/Desktop/VideoPlayer/Audio.cpp" "CMakeFiles/VideoPlayer.dir/Audio.cpp.o" "gcc" "CMakeFiles/VideoPlayer.dir/Audio.cpp.o.d"
+  "/Users/aaronher/Desktop/VideoPlayer/AudioCallback.cpp" "CMakeFiles/VideoPlayer.dir/AudioCallback.cpp.o" "gcc" "CMakeFiles/VideoPlayer.dir/AudioCallback.cpp.o.d"
+  "/Users/aaronher/Desktop/VideoPlayer/Player.cpp" "CMakeFiles/VideoPlayer.dir/Player.cpp.o" "gcc" "CMakeFiles/VideoPlayer.dir/Player.cpp.o.d"
+  "/Users/aaronher/Desktop/VideoPlayer/SDLWrapper.cpp" "CMakeFiles/VideoPlayer.dir/SDLWrapper.cpp.o" "gcc" "CMakeFiles/VideoPlayer.dir/SDLWrapper.cpp.o.d"
+  "/Users/aaronher/Desktop/VideoPlayer/Utils.cpp" "CMakeFiles/VideoPlayer.dir/Utils.cpp.o" "gcc" "CMakeFiles/VideoPlayer.dir/Utils.cpp.o.d"
+  "/Users/aaronher/Desktop/VideoPlayer/main.cpp" "CMakeFiles/VideoPlayer.dir/main.cpp.o" "gcc" "CMakeFiles/VideoPlayer.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/VideoPlayer.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VideoPlayer.dir/Audio.cpp.o"
+  "CMakeFiles/VideoPlayer.dir/Audio.cpp.o.d"
+  "CMakeFiles/VideoPlayer.dir/AudioCallback.cpp.o"
+  "CMakeFiles/VideoPlayer.dir/AudioCallback.cpp.o.d"
+  "CMakeFiles/VideoPlayer.dir/Player.cpp.o"
+  "CMakeFiles/VideoPlayer.dir/Player.cpp.o.d"
+  "CMakeFiles/VideoPlayer.dir/SDLWrapper.cpp.o"
+  "CMakeFiles/VideoPlayer.dir/SDLWrapper.cpp.o.d"
+  "CMakeFiles/VideoPlayer.dir/Utils.cpp.o"
+  "CMakeFiles/VideoPlayer.dir/Utils.cpp.o.d"
+  "CMakeFiles/VideoPlayer.dir/main.cpp.o"
+  "CMakeFiles/VideoPlayer.dir/main.cpp.o.d"
+  "VideoPlayer"
+  "VideoPlayer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VideoPlayer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
